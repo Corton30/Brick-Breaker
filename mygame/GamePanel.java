7cb481d1d2f15044deed5eec46dcb073b0d44678
Corton <abdelrahman.m.hamdy@gmail.com>
2024-05-27 03:57:37 +0200

    added GamePanel

--- mygame/GamePanel.java
+++ mygame/GamePanel.java
@@ -1,4 +1,16 @@
 package mygame;
 
-public class GamePanel {
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
--- mygame/Main.java
+++ mygame/Main.java
@@ -4,8 +4,12 @@ import javax.swing.JFrame;
 
 
 public class Main {
-    public static void main(String[] args){
-        JFrame frame = new JFrame();
-
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Brick Breaker");
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }
