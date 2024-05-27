9cce19a1740bf36a243131ebee64b18df0201c4b
Corton <abdelrahman.m.hamdy@gmail.com>
2024-05-27 04:34:19 +0200

    added Padel and KeyListener

--- mygame/GamePanel.java
+++ mygame/GamePanel.java
@@ -1,16 +1,35 @@
 package mygame;
 
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
 
 public class GamePanel extends JPanel {
+    private Paddle paddle;
+
     public GamePanel() {
+        paddle = new Paddle(350, 500, 100, 10);
+        setFocusable(true);
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_A) {
+                    paddle.moveLeft();
+                    repaint();
+                } else if (key == KeyEvent.VK_D) {
+                    paddle.moveRight();
+                    repaint();
+                }
+            }
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        paddle.draw(g);
     }
 }
--- mygame/Paddle.java
+++ mygame/Paddle.java
@@ -1,4 +1,48 @@
 package mygame;
 
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Rectangle;
+
 public class Paddle {
+    private int x, y, width, height;
+
+    public Paddle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, width, height);
+    }
+
+ //getters and setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x += x ;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+public void moveLeft() {
+        setX(-10);
+    }
+
+public void moveRight() {
+        setX(10);
+    }
 }
